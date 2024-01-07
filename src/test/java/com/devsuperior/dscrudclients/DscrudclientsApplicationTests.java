4039736827d62375c82e0b47ae26f3ca884419b8
mateusluizz <mateusluizz@hotmail.com>
2024-01-06 20:48:52 -0400

    First Commit

---
+++ src/test/java/com/devsuperior/dscrudclients/DscrudclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscrudclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscrudclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
